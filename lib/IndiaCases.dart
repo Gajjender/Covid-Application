d27a97ebf25dc1448823b0d12ff4d1d67c5cf259
Gajjender <rathoregajjender2018@gmail.com>
2020-10-10 13:01:04 +0530

    Container Elements

--- lib/IndiaCases.dart
+++ lib/IndiaCases.dart
@@ -50,25 +50,139 @@ class IndiaCases extends StatelessWidget{
                                       borderData: F1BorderData(show: false),
                                       sections: [
                                         PieChartSectionData(
-                                          showitle: false,
+                                          showTitle: false,
                                           value: double.parse(active),
                                           color: Color.fromRGBO(25, 49, 103, 1), 
-                                          title: "Active",   
+                                          Title: "Active",   
                                         ),
-
-                                         
-                                      ]
-                                    )
+                                        PieChartSectionData(
+                                         showTtile: false,
+                                         Title: "Recovered",
+                                         value: double.parse(recovered),
+                                         color: Theme.of(context).primaryColor,
+                                        ),
+                                        PieChartSectionData(
+                                          showTitle: false,
+                                          Title: "Deaths",
+                                          color: Color.fromRGBO(255, 77, 77, 1),
+                                          value: double.parse(deaths),
+                                        )
+                                      ],
+                                    ),
                                   ),
-                          )                          
+                                ) 
+                              : new Center(
+                                  child: new CircularProgressIndicator()),
+                          Container(
+                            margin: EdgeInsets.only(left: 28),
+                            padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                            child: Column(
+                              mainAxisSize= MainAxisSize.max,
+                              mainAxisAlignment= MainAxisAlignment.end,
+                              crossAxisAlignment= CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.all(5),
+                                      color: Color.fromRGBO(25, 49, 103, 1),
+                                      height: 20,
+                                      width: 20,
+                                    ),
+                                    Text(
+                                      "Active"
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Theme.of(context).primaryColor,
+                                      )
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.all(5),
+                                      color: Color.fromRGBO(255, 77, 77, 1),
+                                      height: 20,
+                                      width: 20,
+                                    ),
+                                    Text(
+                                      "Deaths"
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Theme.of(context).primaryColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.all(5),
+                                      color: Theme.of(context).primaryColor,
+                                      height: 20,
+                                      width: 20,
+                                    ),
+                                    Text(
+                                      "Recovered"
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Theme.of(context).primaryColor,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ]
+                            ),
+                          ),
                         ]
-                      ),        
+                      ), 
                     )
                   )
                 ]
               ),
             ),
           ),
+          if(isState)
+            Container(
+                margin: EdgeInsets.fromLTRB(15, 5, 15, 20),
+                height: ((MediaQuery.of(context).size.height)-
+                        (MediaQuery.of(context).padding.top))*
+                    0.1,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Card(
+                        elevation: 5,
+                        color: Color.fromRGBO(230, 230, 230, 1),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "State Name",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w900,
+                                color: Theme.of(context).primaryColor,
+                                fontSize: 17)
+                            ),
+                            stateName!=null
+                                ? Text(
+                                    stateName,
+                                    style: TextStyle(
+                                      color: Theme.of(context).primaryColor,
+                                    ),
+                                )
+                              : Text("----"),  
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              )),
+          Container(
+            //We'll Start from here tomorrow
+          )    
         ]
       ),
     );
